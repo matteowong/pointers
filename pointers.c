d336650ad6d54c7b2ad034c7b57dacd29535a556
matteowong <mwong8@stuy.edu>
2017-09-20 17:47:19 -0400

    pointers

---
+++ pointers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  srand(time(NULL));
+  int arr[10];
+  int i=0;
+  for (;i<9;i++)
+    arr[i]=rand();
+  arr[9]=0;
+  for (i=0;i<10;i++)
+    printf("arr[%d]: %d\n",i,arr[i]);
+  int reverse[10];
+  int * ip= &arr[9];
+  int * rp=&reverse[0];
+  while (rp<=&reverse[9]) {
+    *rp=*ip;
+    ip--;
+    rp++;
+  }
+  for (i=0;i<10;i++)
+    printf("reverse[%d]: %d\n",i,reverse[i]);
+  return 0;
+}
