f83c7268ac5f2b756e6892de1a40e1ec0077d313
matteowong <mwong8@stuy.edu>
2017-09-26 11:31:48 -0400

    printarray fx

--- pointers.c
+++ pointers.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+int print_array(int * a, int length) {
+  int i;
+  for (i=0;i<length;i++)
+    printf("element %d: %d\n",i,a[i]);
+}
+
 int main() {
   srand(time(NULL));
   int arr[10];
@@ -9,12 +15,11 @@ int main() {
   for (;i<9;i++)
     arr[i]=rand();
   arr[9]=0;
-  for (i=0;i<10;i++)
-    printf("arr[%d]: %d\n",i,arr[i]);
+  print_array(arr,10);
   int reverse[10];
-  int * ip= &arr[9];
-  int * rp=&reverse[0];
-  while (rp<=&reverse[9]) {
+  int * ip= arr+9;
+  int * rp=reverse;
+  while (rp<=reverse+9) {
     *rp++=*ip--;
   }
   for (i=0;i<10;i++)
